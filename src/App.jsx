55fca1ff4535b62288db80d8ad327ff4c76f89d5
The-King-12345 <alexseidinger15@gmail.com>
2024-07-19 08:45:01 -0700

    Organize field markings into a new Markings component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react"
-import Hash from "./components/Hash"
-import Sideline from "./components/Sideline"
-import Yardline from "./components/Yardline"
-import Number from "./components/Number"
+import Markings from "./components/Markings"
 import Dot from "./components/Dot"
 import useFetch from "./hooks/useFetch"
 import './App.css'
@@ -20,12 +17,7 @@ function App() {
 
   return (
     <main>
-      <Hash top={100/3}/>
-      <Hash top={200/3}/>
-      <Yardline />
-      <Sideline />
-      <Number top="15.25" flipped={true}/>
-      <Number top="84.75"/>
+      <Markings></Markings>
 
       {filteredData.map((performer) => (
         <Dot key={performer.id} top={performer.Front_hash + performer.Front_steps*25/21} left={performer.Side_yd + performer.Side_steps*5/8}/>
