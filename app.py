015999f534f1a3581eedd879e33f07b647128be6
The-King-12345 <alexseidinger15@gmail.com>
2024-07-21 16:28:58 -0700

    Create next and prev button to cycle through sets

--- app.py
+++ app.py
@@ -7,23 +7,36 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
+# global variable sets
+sets = 0
+MAX = 28
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+    global sets
+
     if request.method == "POST":
-        pass
+        action = request.form.get('action')
+        if action == "prev" and sets > 0:
+            sets -= 1
+        elif action == "next" and sets < MAX:
+            sets += 1
+        
+        return redirect("/")
+    
     else:
         conn = sqlite3.connect("database.db")
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM performers WHERE sets=0")
+        cursor.execute(f"SELECT * FROM performers WHERE sets={sets}")
         rows = cursor.fetchall()
         dots = [dict(row) for row in rows]
 
         cursor.close()
         conn.close()
 
-        return render_template("index.html", dots=dots)
+        return render_template("index.html", dots=dots, sets=sets)
 
 if __name__ == '__main__':
     app.run(debug=True)
