86b893fd8b0ac55ee824c10721312f23f2e3c0a4
The-King-12345 <alexseidinger15@gmail.com>
2024-07-23 11:55:00 -0700

    Update databse structure and implement session variable
    Split 1 table into 3 tables for less repetition by using foreign keys. Replace global variable with a session variable to give different users independent control over the site

--- app.py
+++ app.py
@@ -1,42 +1,56 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 import sqlite3
-import logging
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-# global variable page
-page = 0
-MAX = 28
+# global variable 
+MIN = 1
+MAX = 30
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    global page
+    page = session.get("page", 1)
 
-    if request.method == "POST":
-        action = request.form.get('action')
-        if action == "prev" and page > 0:
-            page -= 1
-        elif action == "next" and page < MAX:
-            page += 1
-        
-        return redirect("/")
-    
-    else:
-        conn = sqlite3.connect("database.db")
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
 
-        cursor.execute(f"SELECT * FROM performers WHERE sets={page}")
-        rows = cursor.fetchall()
-        dots = [dict(row) for row in rows]
+    # get pages
+    try:
+        cursor.execute(f"SELECT * FROM pages WHERE id = {page}")
+    except sqlite3.Error as e:
+        app.logger.info(f"ERROR at get_dots: {e}")
+    pages = dict(cursor.fetchone())
 
-        cursor.close()
-        conn.close()
+    # get dots
+    try:
+        cursor.execute(f"SELECT * FROM dots WHERE page_id = {page}")
+    except sqlite3.Error as e:
+        app.logger.info(f"ERROR at get_dots: {e}")
+    dots = [dict(row) for row in cursor.fetchall()]
 
-        return render_template("index.html", dots=dots, page=page)
+    cursor.close()
+    conn.close()
+
+    return render_template("index.html", dots=dots, pages=pages)
+
+
+@app.route('/increment', methods=['POST'])
+def increment():
+
+    action = request.form.get("action")
+    if action == "prev" and session.get("page", 1) > MIN:
+        page = session.get("page", 1) - 1
+        session["page"] = page
+    elif action == "next" and session.get("page", 1) < MAX:
+        page = session.get("page", 1) + 1
+        session["page"] = page
+
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- populate_table.py
+++ populate_table.py
@@ -1,74 +1,139 @@
 import sqlite3
 import pdfplumber
 import re
+import argparse
+
+PDF_PATH = "pdf.pdf"
+DATABASE_PATH = "database.db"
+TEXT_PATH = "text.txt"
 
 def main():
-    text = extract_text_from_pdf("pdf.pdf")
-    populate(text)
+    parser = argparse.ArgumentParser(description="Populate a database using information from a pdf")
+    parser.add_argument("-c", "--create", action="store_true", help="Create blank tables in the database (clears existing tables)")
+    parser.add_argument("-t", "--text", action="store_true", help="Extract text from pdf and store it in a text file")
+    parser.add_argument("-p", "--populate", action="store_true", help="Populate the database using the text file")
 
-def populate(text):
-    conn = sqlite3.connect("database.db")
+    args = parser.parse_args()
+
+    # Run all if no args provided
+    if not any(vars(args).values()):
+        args.text = True
+        args.create = True
+        args.populate = True
+
+    if args.create:
+        create_tables(DATABASE_PATH)
+    if args.text:
+        text = extract_text_from_pdf(PDF_PATH)
+        create_text_file(TEXT_PATH, text)
+    if args.populate:
+        populate(DATABASE_PATH, TEXT_PATH)
+
+
+def populate(db_path, txt_path):
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    lines = text.split('\n')
-    current_symbol = "MISSING"
-    for line in lines:
-        if matches := re.search(r"^(\d+A?) (?:\d+\-\d+)? ?(\d+) Side ([12]): (?:(On)|([\d\.]+) steps (inside|outside)) (\d+) yd ln (?:(On)|([\d\.]+) steps (in front of|behind)) (.+)$", line):            
-            sets = matches.group(1)
-            counts = int(matches.group(2))
-            side = int(matches.group(3))
-            yd = int(matches.group(7))
+    with open(txt_path, "r") as txt_file:
+        current_symbol = "MISSING"
+        lines = txt_file.readlines()
+        for line in lines:
 
-            if matches.group(4) != None:
-                yd_steps = 0
-            else:
-                if matches.group(6) == "inside":
-                    yd_steps = float(matches.group(5))
-                elif matches.group(6) == "outside":
-                    yd_steps = float(matches.group(5)) * -1
+            # populate performers
+            if matches := re.search(r"Performer: Symbol: ([A-Z]) Label: (\d+)", line):
+                try:
+                    cursor.execute("INSERT INTO performers (name, symbol, label) VALUES(?, ?, ?)", (matches.group(1) + matches.group(2), matches.group(1), matches.group(2)))
+                    conn.commit()
+                except sqlite3.Error as e:
+                    print(f"ERROR at populate_performers: {e}")
+            
+            # populate dots and pages
+            elif matches := re.search(r"^(\d+A?) (\d+\-\d+)? ?(\d+) Side ([12]): (?:(On)|([\d\.]+) steps (inside|outside)) (\d+) yd ln (?:(On)|([\d\.]+) steps (in front of|behind)) (.+)$", line):            
+                page = matches.group(1)
+                measures = matches.group(2)
+                counts = int(matches.group(3))
+                side = int(matches.group(4))
+                yd = int(matches.group(8))
+
+                # set yd_steps
+                if matches.group(5) != None:
+                    yd_steps = 0
                 else:
-                    raise ValueError(f"ERROR setting yd_steps at {matches.group(6)}")
+                    if matches.group(7) == "inside":
+                        yd_steps = float(matches.group(6))
+                    elif matches.group(7) == "outside":
+                        yd_steps = float(matches.group(6)) * -1
+                    else:
+                        raise ValueError(f"ERROR setting yd_steps at {matches.group(7)}")
                 
-            if matches.group(8) != None:
-                hash_steps = 0
-            else:
-                if matches.group(10) == "in front of":
-                    hash_steps = float(matches.group(9)) 
-                elif matches.group(10) == "behind":
-                    hash_steps = float(matches.group(9)) * -1
+                # set hash_steps
+                if matches.group(9) != None:
+                    hash_steps = 0
                 else:
-                    raise ValueError(f"ERROR setting hash_steps at {matches.group(10)}")
+                    if matches.group(11) == "in front of":
+                        hash_steps = float(matches.group(10)) 
+                    elif matches.group(11) == "behind":
+                        hash_steps = float(matches.group(10)) * -1
+                    else:
+                        raise ValueError(f"ERROR setting hash_steps at {matches.group(11)}")
+                    
+                # set hash
+                if matches.group(12) == "Front side line":
+                    hash = 100
+                elif matches.group(12) == "Front Hash (HS)":
+                    hash = 66.6666
+                elif matches.group(12) == "Back Hash (HS)":
+                    hash = 33.3333
+                elif matches.group(12) == "Back side line":
+                    hash = 0
+                else:
+                    raise ValueError(f"ERROR setting hash at {matches.group(12)}")
                 
-            if matches.group(11) == "Front side line":
-                hash = 100
-            elif matches.group(11) == "Front Hash (HS)":
-                hash = 66.6666
-            elif matches.group(11) == "Back Hash (HS)":
-                hash = 33.3333
-            elif matches.group(11) == "Back side line":
-                hash = 0
-            else:
-                raise ValueError(f"ERROR setting hash at {matches.group(11)}")
+                # populate pages
+                try:
+                    cursor.execute(f"SELECT EXISTS (SELECT 1 FROM pages WHERE page = '{page}')")
+                except sqlite3.Error as e:
+                    print(f"ERROR at select_exists: {e}")
 
-            try:
-                cursor.execute("INSERT INTO performers (symbol, sets, counts, side, yd_steps, yd, hash_steps, hash) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (current_symbol, sets, counts, side, yd_steps, yd, hash_steps, hash))
-                conn.commit()
-            except sqlite3.Error as e:
-                print(f"ERROR: {e}")
+                if cursor.fetchone()[0] == False:
+                    try:
+                        cursor.execute("INSERT INTO pages (page, measures, counts) VALUES(?, ?, ?)", (page, measures, counts))
+                        conn.commit()
+                    except sqlite3.Error as e:
+                        print(f"ERROR at populate_pages: {e}")
+                
+                # get performer_id
+                try:
+                    cursor.execute("SELECT id FROM performers ORDER BY id DESC LIMIT 1")
+                except sqlite3.Error as e:
+                    print(f"ERROR at get_performer_id: {e}")
+                performer_id = cursor.fetchone()[0]
 
+                # get page_id
+                try:
+                    cursor.execute(f"SELECT id FROM pages WHERE page = '{page}' LIMIT 1")
+                except sqlite3.Error as e:
+                    print(f"ERROR at get_page_id: {e}")
+                page_id = cursor.fetchone()[0]
 
-        elif matches := re.search(r"Performer: Symbol: ([A-Z]) Label: (\d+)", line):
-            current_symbol = matches.group(1) + matches.group(2)
+                # populate dots
+                try:
+                    cursor.execute("INSERT INTO dots (performer_id, page_id, side, yd_steps, yd, hash_steps, hash) VALUES(?, ?, ?, ?, ?, ?, ?)", (performer_id, page_id, side, yd_steps, yd, hash_steps, hash))
+                    conn.commit()
+                except sqlite3.Error as e:
+                    print(f"ERROR at populate_dots: {e}")
+
     
     print("Data inserted successfully")
+
+    cursor.close()
     conn.close()
+    return
 
 
-
-
-def extract_text_from_pdf(pdf_path):
+def extract_text_from_pdf(path):
     text = []
-    with pdfplumber.open(pdf_path) as pdf:
+    with pdfplumber.open(path) as pdf:
         for page in pdf.pages:
             width, height = page.width, page.height
             quarters = [
@@ -82,6 +147,63 @@ def extract_text_from_pdf(pdf_path):
                 text.append(cropped_page.extract_text())
     return "\n".join(text)
 
+
+def create_text_file(path, text):
+    with open(path, "w") as file:
+        file.write(text)
+        
+    print("TEXT file created successfully.")
+    return 
+
+
+def create_tables(path):
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+
+    cursor.execute("DROP TABLE performers")
+    cursor.execute("DROP TABLE pages")
+    cursor.execute("DROP TABLE dots")
+    print("Tables cleared successfully.")
+
+    cursor.execute('''
+        CREATE TABLE performers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE NOT NULL,
+            symbol TEXT NOT NULL,
+            label INTEGER NOT NULL
+        )
+    ''')
+    
+    cursor.execute('''
+        CREATE TABLE pages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            page TEXT UNIQUE NOT NULL,
+            measures TEXT,
+            counts INTEGER NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE dots (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            performer_id INTEGER NOT NULL,
+            page_id INTEGER NOT NULL,   
+            side INTEGER NOT NULL,
+            yd_steps REAL NOT NULL,
+            yd INTEGER NOT NULL,
+            hash_steps REAL NOT NULL, 
+            hash REAL NOT NULL,
+            FOREIGN KEY (performer_id) REFERENCES performers(id),
+            FOREIGN KEY (page_id) REFERENCES pages(id)
+        )
+    ''')
+
+    print("Tables created successfully.")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return
 
 
 if __name__ == "__main__":
