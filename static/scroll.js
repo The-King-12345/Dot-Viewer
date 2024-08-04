fc54d83b90810fcaffa4830041458ae5adfce612
The-King-12345 <alexseidinger15@gmail.com>
2024-08-04 11:33:08 -0700

    Add max scroll

--- static/scroll.js
+++ static/scroll.js
@@ -6,24 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     container.addEventListener("wheel", (event) => {
-        event.preventDefault();
+        if (event.ctrlKey) {
+            event.preventDefault();
 
-        // Update scale factor based on scroll direction
-        if (event.deltaY < 0) {
-            scale += scaleIncrement;
-        } else if (event.deltaY > 0) {
-            scale = Math.max(scaleIncrement, scale - scaleIncrement); // Prevent scale from going below a minimum value
+            // Update scale factor based on scroll direction
+            if (event.deltaY < 0) {
+                scale += scaleIncrement;
+            } else if (event.deltaY > 0) {
+                scale = Math.max(1, scale - scaleIncrement); // Prevent scale from going below a minimum value
+            }
+
+            // Apply the scale transform
+            zoomableFlexbox.style.transform = `scale(${scale})`;
+
+            const containerWidth = container.clientWidth;
+            const containerHeight = container.clientHeight;
+            const contentWidth = zoomableFlexbox.clientWidth * scale;
+            const contentHeight = zoomableFlexbox.clientHeight * scale;
+
+            // Center the scroll position after zooming
+            window.scrollTo((contentWidth - containerWidth) / 2, (contentHeight - containerHeight) / 2);
         }
-
-        // Apply the scale transform
-        zoomableFlexbox.style.transform = `scale(${scale})`;
-
-        const containerWidth = container.clientWidth;
-        const containerHeight = container.clientHeight;
-        const contentWidth = zoomableFlexbox.clientWidth * scale;
-        const contentHeight = zoomableFlexbox.clientHeight * scale;
-
-        // Center the scroll position after zooming
-        window.scrollTo((contentWidth - containerWidth) / 2, (contentHeight - containerHeight) / 2);
     });
 });
